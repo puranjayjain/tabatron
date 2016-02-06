9ab400c6d0d09d507e5292c76ae0fa75a1763de6
puru1joy <puru1joy@gmail.com>
2016-02-06 13:42:16 +0530

    added mr tabba

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,54 @@
+//this guy runs for the life of a tab
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+    // console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.webNavigation.onCommitted.addListener(function(data) {
+    //NOTE if an actual page is opened
+    if (data.frameId === 0) {
+        console.log(data);
+        if (data.transitionType === 'typed') {
+            console.log('yo just typed it');
+        }
+    }
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+//     if (typeof data){
+//         var navData = (chrome.i18n.getMessage('inHandler'), this, data);
+//         console.log();
+//     }
+//     else{
+//         console.error(chrome.i18n.getMessage('inHandlerError'), this);
+//     }
+});
+
+// chrome.browserAction.setBadgeText({text: '2'});
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+//     console.log(details);
+// });
+
+// var nav = new NavigationCollector();
+// chrome.webNavigation['onBeforeNavigate'].addListener(function(data) {
+//     if (typeof data){
+//         var navData = (chrome.i18n.getMessage('inHandler'), this, data);
+//         console.log();
+//     }
+//     else{
+//         console.error(chrome.i18n.getMessage('inHandlerError'), this);
+//     }
+// });
+//
+// var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+//     'onCommitted', 'onCompleted', 'onDOMContentLoaded',
+//     'onErrorOccurred', 'onReferenceFragmentUpdated', 'onTabReplaced',
+//     'onHistoryStateUpdated'];
+//
+// eventList.forEach(function(e) {
+//   chrome.webNavigation[e].addListener(function(data) {
+//     if (typeof data)
+//       console.log(chrome.i18n.getMessage('inHandler'), e, data);
+//     else
+//       console.error(chrome.i18n.getMessage('inHandlerError'), e);
+//   });
+// });
