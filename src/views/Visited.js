ca953236c62e23181af6209cd0a300d1216e7476
Puranjay Jain <puranjay.jain@st.niituniversity.in>
2016-07-17 22:00:06 +0530

    building main timeline view

--- src/views/Visited.js
+++ src/views/Visited.js
@@ -2,10 +2,30 @@ import React, {PropTypes, Component} from 'react'
 
 export default class Visited extends Component {
   render() {
+    const style = {
+      container: {
+        display: 'flex',
+        flexDirection: 'column'
+      },
+      timeline: {
+        background: this.context.muiTheme.palette.accent1Color,
+        height: 2,
+        width: '100%',
+        marginTop: 34
+      }
+    }
+
     return (
-      <div>
-        some page
+      <div style={style.container}>
+        <div style={style.timeline}></div>
+        <div>
+          tabs are here
+        </div>
       </div>
     )
   }
+}
+
+Visited.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
 }
